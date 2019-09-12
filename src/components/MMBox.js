65a8bd944bcf2ffa2ca44831e4ea082e6571e965
SlarDptor <eljuanchosirk@gmail.com>
2019-09-12 02:31:01 -0300

    CSS ordenado y estilos de home

--- src/components/MMBox.js
+++ src/components/MMBox.js
@@ -2,12 +2,11 @@ import React from "react";
 
 function MMBox() {
   return (
-    <div fluid={true} className="mm-box">
-      <h1 className="mm">MARIANO MEDINA</h1>
-      <p className="description">
+    <div className="mm-box">
+      <h1 className="mm-title">JUAN PABLO MEDINA</h1>
+      <p className="mm-description">
         Irure proident ex aliquip nisi ad laborum commodo elit aute dolor eu
-        esse. Mollit ex anim est do ipsum eu cupidatat sunt qui. Eiusmod duis ut
-        voluptate aute elit est laboris culpa.
+        esse. Mollit ex anim est do ipsum eu cupidatat sunt qui.
       </p>
     </div>
   );
--- src/components/Structure.js
+++ src/components/Structure.js
@@ -2,6 +2,7 @@ import React from "react";
 import TopNavBar from "./TopNavBar";
 import Container from "react-bootstrap/Container";
 import MMBox from "./MMBox";
+import Button from "react-bootstrap/Button";
 
 function Structure() {
   //Values.
@@ -20,7 +21,9 @@ function Structure() {
             id="firstSectionContent"
             className="mobile-container first-section-content"
           >
+            <div className="white-line" />
             <MMBox />
+            <Button className="get-started">{"LET'S GET STARTED"}</Button>
           </Container>
         </div>
       </div>
